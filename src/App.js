2ea6dab22d4ee86a2ca1b6430c2599f912928b17
Thang0226 <thang.nd0226@gmail.com>
2025-01-23 12:06:24 +0700

    finish simple routes

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+
+        <hr/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </div>
+  );
+}
+
+export default App;
